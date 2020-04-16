94559740fbe2fb9a35d24db5867295d4c16c7441
NanyoungKim <56589325+NanyoungKim@users.noreply.github.com>
2020-04-16 23:27:45 +0900

    Socket communication between server and client
    server : socket() -> bind() -> listen() -> accept() -> read() and write() -> close()
    client : socket() -> connect() -> read() and write() -> close()

---
+++ tcp-client.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main(){
+	// variables
+	int socketFd;
+	struct sockaddr_in serverAddress;
+	
+	// socket creation
+	socketFd = socket(AF_INET, SOCK_STREAM, 0);
+	if(socketFd == -1) {
+		printf("failed to create a socket\n");
+		exit(1);
+	} else {
+		printf("socket : %d\n", socketFd);
+	}
+	
+	// connect
+	bzero(&serverAddress, sizeof(serverAddress));		//claering out the server address variable and we're setting all the bytes to zero first.
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serverAddress.sin_port = htons(4444);
+	if(connect(socketFd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) != 0) {
+		printf("failed to connect\n");
+		exit(1);
+	} else {
+		printf("connected\n");
+	}
+	
+	// read
+	char buf[128];
+	read(socketFd, buf, 128);
+	printf("received : %s\n", buf);
+	
+	// close
+	close(socketFd);
+	printf("socket closed");
+	
+	return 0;
+}
---
+++ tcp-server.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main(){
+	// variables
+	int serverSocketFd, clientSocketFd;
+	struct sockaddr_in serverAddress, clientAddress;
+	int clientAddressLength;
+	
+	// socket creation
+	serverSocketFd = socket(AF_INET, SOCK_STREAM, 0);
+	if(serverSocketFd == -1) {
+		printf("failed to create a socket\n");
+		exit(1);
+	} else {
+		printf("socket : %d\n", serverSocketFd);
+	}
+	
+	// bind
+	//家南苞 家南林家 官牢爹秦林妨搁 刚历 家南林家刚历 specify秦具茄促.
+	bzero(&serverAddress, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddress.sin_port = htons(4444);
+	if(bind(serverSocketFd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) != 0) {
+		printf("failed to bind\n");
+		exit(1);
+	} else {
+		printf("bound\n");
+	}
+	
+	// listening
+	if(listen(serverSocketFd, 1) != 0) {
+		printf("failed to listen\n");
+		exit(1);
+	} else {
+		printf("listening...\n");
+	}
+	
+	// accepting
+	bzero(&clientAddress, sizeof(clientAddress));
+	clientSocketFd = accept(serverSocketFd, (struct sockaddr*)&clientAddress, &clientAddressLength);
+	if(clientSocketFd == -1) {
+		printf("failed to accept\n");
+		exit(1);
+	} else {
+		printf("accepted\n");
+	}
+	
+	// writing (communicating)
+	char* msg = "Hello World!";
+	write(clientSocketFd, msg, strlen(msg));
+	printf("sent : \"%s\"\n", msg);
+	
+	// close
+	close(clientSocketFd);
+	printf("client socket closed\n");
+	close(serverSocketFd);
+	printf("server socket closed\n");
+	
+	return 0;
+}
